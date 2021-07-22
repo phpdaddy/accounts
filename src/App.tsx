1e86ff3327810eb6cf3166fd980fa9f6e7843764
Maksym Prysiazhnyi <maksym.prysiazhnyi@valatron.com>
2021-07-22 17:08:47 +0200

    Add delete

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import {Col, Form, Nav, Row, Tab, Tabs} from "react-bootstrap";
 
 function App() {
     const [views, setViews] = useState([] as any[]);
-    const columns = [{id: 'created', label: 'Created'}, {id: 'accountNumber', label: 'Account number'},
+    const columns = [{id: 'created', label: 'Created'},
+        {id: 'accountNumber', label: 'Account number'},
         {id: 'accountType', label: 'Account type'},
         {id: 'clientName', label: 'Client name'},
         {id: 'balance', label: 'Balance'}];
 
-    return <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+    return <Tab.Container defaultActiveKey="first">
         <Row>
             <Col sm={3}>
                 <Nav variant="pills" className="flex-column">
@@ -27,7 +28,7 @@ function App() {
 
                         <Tabs defaultActiveKey="0" id="uncontrolled-tab-example" className="mb-3">
                             {views.map((v, index) => {
-                                return <Tab eventKey={index} title={views[index].name || `View ${index}`}>
+                                return <Tab key={index} eventKey={index} title={views[index].name || `View ${index}`}>
                                     <Form>
                                         <Form.Group className="mb-3">
                                             <Form.Label>Name</Form.Label>
@@ -35,7 +36,7 @@ function App() {
                                                           defaultValue={views[index].name}
                                                           onChange={(e) => {
                                                               views[index].name = e.target.value;
-                                                              setViews(views);
+                                                              setViews([...views]);
                                                           }}/>
                                         </Form.Group>
 
@@ -45,7 +46,6 @@ function App() {
                                                 return <Form.Check name={c.id} key={cIndex} type="checkbox"
                                                                    label={c.label} defaultValue={c.id}
                                                                    onChange={(e) => {
-                                                                       console.log(index);
                                                                        if (!views[index].columns) {
                                                                            views[index].columns = [];
                                                                        }
@@ -55,7 +55,7 @@ function App() {
                                                                            views[index].columns
                                                                                = views[index].columns.filter((c2: string) => c2 !== c.id);
                                                                        }
-                                                                       setViews(views);
+                                                                       setViews([...views]);
                                                                    }}/>;
                                             })}
 
@@ -65,7 +65,7 @@ function App() {
 
                                             <Form.Select defaultValue={views[index].sortBy} onChange={(e) => {
                                                 views[index].sortBy = (e.target as HTMLInputElement).value;
-                                                setViews(views);
+                                                setViews([...views]);
                                             }}>
                                                 {columns.map((c, cIndex) => {
                                                     return <option key={c.id} value={c.id}>{c.label}</option>;
@@ -74,7 +74,7 @@ function App() {
                                             </Form.Select>
                                             <Form.Select defaultValue={views[index].sortByOrder} onChange={(e) => {
                                                 views[index].sortByOrder = (e.target as HTMLInputElement).value;
-                                                setViews(views);
+                                                setViews([...views]);
                                             }}>
                                                 <option value="asc">Ascending</option>
                                                 <option value="asc">Descending</option>
@@ -88,9 +88,21 @@ function App() {
                                                           defaultValue={views[index].pageSize}
                                                           onChange={(e) => {
                                                               views[index].pageSize = e.target.value;
-                                                              setViews(views);
+                                                              setViews([...views]);
                                                           }}/>
                                         </Form.Group>
+
+
+                                        <button onClick={() => {
+
+
+                                        }}>Save
+                                        </button>
+
+                                        <button onClick={() => {
+                                            setViews([...views.filter((v, i) => i === index)]);
+                                        }}>Delete
+                                        </button>
                                     </Form>
                                 </Tab>;
                             })}
